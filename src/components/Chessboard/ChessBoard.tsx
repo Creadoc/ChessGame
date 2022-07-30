048d745875204e8d4a6e067eebdfb7fbdf92e26a
Creadoc <jcoll06t@gmail.com>
2022-07-30 15:38:55 -0500

    pieces are displayed on the board now.  Time for mechanics next:

--- src/components/Chessboard/ChessBoard.tsx
+++ src/components/Chessboard/ChessBoard.tsx
@@ -13,9 +13,24 @@ interface Piece {
 
 const pieces: Piece[] = [];
 
-for (let i = 0; i < verticalAxis.length; i++) {
-  pieces.push({ image: "../../assets/pawn_b.png", x: i, y: 6 });
-  pieces.push({ image: "../../assets/pawn_w.png", x: i, y: 1 });
+//other pieces rendering:
+for (let p = 0; p < 2; p++) {
+  const type = p === 0 ? "b" : "w";
+  const y = p === 0 ? 7 : 0;
+  pieces.push({ image: `./assets/rook_${type}.png`, x: 0, y: y });
+  pieces.push({ image: `./assets/knight_${type}.png`, x: 1, y: y });
+  pieces.push({ image: `./assets/bishop_${type}.png`, x: 2, y: y });
+  pieces.push({ image: `./assets/queen_${type}.png`, x: 3, y: y });
+  pieces.push({ image: `./assets/king_${type}.png`, x: 4, y: y });
+  pieces.push({ image: `./assets/bishop_${type}.png`, x: 5, y: y });
+  pieces.push({ image: `./assets/knight_${type}.png`, x: 6, y: y });
+  pieces.push({ image: `./assets/rook_${type}.png`, x: 7, y: y });
+}
+
+//pawns rendering:
+for (let i = 0; i < horizontalAxis.length; i++) {
+  pieces.push({ image: "./assets/pawn_b.png", x: i, y: 6 });
+  pieces.push({ image: "./assets/pawn_w.png", x: i, y: 1 });
 }
 
 export default function ChessBoard() {
@@ -25,18 +40,15 @@ export default function ChessBoard() {
     for (let i = 0; i < horizontalAxis.length; i++) {
       let image = undefined;
 
+      //putting the pieces on the tiles of the board:
       pieces.forEach((p) => {
         if (p.x === i && p.y === j) {
           image = p.image;
         }
       });
 
-      board.push(
-        <Tile
-          image="/Users/jimmycollins/Desktop/chessgame/src/components/Chessboard/bishop_b.png"
-          number={i + j}
-        />
-      );
+      //if there is no piece there, just render the tile without a piece on it:
+      board.push(<Tile image={image} number={i + j} />);
     }
   }
   return <div id="chessboard">{board}</div>;
